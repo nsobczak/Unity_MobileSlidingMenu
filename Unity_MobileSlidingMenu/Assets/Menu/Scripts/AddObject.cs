1fbeab30faded7020b429ce5b36306343908f85b
nsobczak <nicolas.sobczak@orange.fr>
2020-02-26 16:07:16 +0100

    Added 1st version of the menu

---
+++ Unity_MobileSlidingMenu/Assets/Menu/Scripts/AddObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AddObject : MonoBehaviour
+{
+    #region Parameters
+    [SerializeField] private String tagClickable = "ClickableObject";
+    [SerializeField] private GameObject[] prefabObjModels;
+    [SerializeField] private Toggle[] toggle;
+
+    private GameObject obj;
+    #endregion
+
+
+    //____________________________________________________________________
+
+    #region Buttons_functions
+
+    public void AddObjectButtonClicked()
+    {
+        //trouver l'index du toggle actif
+        int objectModelIndex = 0;
+        //while (!toggle[objectModelIndex].isOn && objectModelIndex < toggle.Length)
+        //    objectModelIndex++;
+
+        //faire un switch sur l'index+1
+        obj = prefabObjModels[objectModelIndex];
+        Debug.Log("obj: " + obj);
+
+        if (null != obj)
+        {
+            Vector3 newPosition = new Vector3(0, 0, 0);
+            Quaternion newRotation = Quaternion.Euler(0, 0, 0);
+            GameObject currentObj = Instantiate(obj, newPosition, newRotation);
+            currentObj.tag = tagClickable;
+            //ObjControl.objList.Add(obj);
+        }
+        else
+            Debug.Log("obj is null");
+    }
+    #endregion
+}
