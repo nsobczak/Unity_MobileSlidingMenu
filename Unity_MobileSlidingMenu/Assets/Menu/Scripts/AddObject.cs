b7384d2204aa845da79e71026d89ca4304c601a6
nsobczak <nicolas.sobczak@orange.fr>
2020-02-26 17:51:30 +0100

    Added choice for object to spawn

--- Unity_MobileSlidingMenu/Assets/Menu/Scripts/AddObject.cs
+++ Unity_MobileSlidingMenu/Assets/Menu/Scripts/AddObject.cs
@@ -4,38 +4,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class AddObject : MonoBehaviour
 {
     #region Parameters
     [SerializeField] private String tagClickable = "ClickableObject";
-    [SerializeField] private GameObject[] prefabObjModels;
-    [SerializeField] private Toggle[] toggle;
+    [SerializeField] private Text objToSpawnName;
+    [SerializeField] private GameObject[] prefabs;
 
-    private GameObject obj;
+    private int _objToSpawnIdx;
     #endregion
 
 
     //____________________________________________________________________
 
+    void Start()
+    {
+        _objToSpawnIdx = 0;
+        UpdateNameObjToSpawn();
+    }
+
+    //____________________________________________________________________
+
     #region Buttons_functions
+
+    public void UpdateNameObjToSpawn()
+    {
+        objToSpawnName.text = prefabs[_objToSpawnIdx].name;
+    }
+
+    public void SwitchObjToSpawn(bool increment = true)
+    {
+        _objToSpawnIdx += (increment ? 1 : -1);
+        if (_objToSpawnIdx <= 0)
+            _objToSpawnIdx = prefabs.Length - 1;
+        else if (_objToSpawnIdx >= prefabs.Length)
+            _objToSpawnIdx = 0;
+
+        UpdateNameObjToSpawn();
+    }
 
     public void AddObjectButtonClicked()
     {
-        //trouver l'index du toggle actif
-        int objectModelIndex = 0;
-        //while (!toggle[objectModelIndex].isOn && objectModelIndex < toggle.Length)
-        //    objectModelIndex++;
-
-        //faire un switch sur l'index+1
-        obj = prefabObjModels[objectModelIndex];
+        GameObject obj = prefabs[_objToSpawnIdx];
         Debug.Log("obj: " + obj);
 
         if (null != obj)
         {
-            Vector3 newPosition = new Vector3(0, 0, 0);
-            Quaternion newRotation = Quaternion.Euler(0, 0, 0);
-            GameObject currentObj = Instantiate(obj, newPosition, newRotation);
+            Transform trans = obj.GetComponent<Transform>();
+            GameObject currentObj = Instantiate(obj, trans.position, trans.rotation) ;
             currentObj.tag = tagClickable;
             //ObjControl.objList.Add(obj);
         }
